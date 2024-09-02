63041ddac5971e457d2333371fc3eba08ef9f400
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-09-02 21:41:33 +0530

    leaflet js

--- public/js/script.js
+++ public/js/script.js
@@ -14,11 +14,12 @@ if(navigator.geolocation){
     })
 }
 
-const map = L.map("map").setView([0,0], 50);
+const map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "OpenStreetMap"
-}).addTo(map)
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
 
 const markers = {};
@@ -41,3 +42,10 @@ socket.on("user-disconnected", (id) => {
         delete markers[id]
     }
 })
+
+
+
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
